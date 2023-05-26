db65db4cd9aa78000b69de3086297882a1f2d18f
«SimLove» <simonolyuba@yandex.ru>
2023-05-26 20:00:56 +0300

    add task2

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int second=(number/10)%10;
+Console.WriteLine("Вторая цифра - "+second);
