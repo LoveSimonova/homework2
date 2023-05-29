5288a6f8a3eb541fa0ac1e42587dca2837f38823
«SimLove» <simonolyuba@yandex.ru>
2023-05-29 15:52:18 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>99)
+{
+    int first=(number/100);
+    while (first>9)
+    {
+        number=number/10;
+        first=(number/100);
+    }
+    int third = number%10;
+    Console.WriteLine("Третья цифра - "+third);
+}
+else
+    Console.WriteLine("Третьей цифры нет");
