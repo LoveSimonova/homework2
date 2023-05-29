41f3b6be162582f1387ad4245f31d83561fa0e07
«SimLove» <simonolyuba@yandex.ru>
2023-05-29 15:42:49 +0300

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число, обозначающее день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==6||number==7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Рабочий день");
